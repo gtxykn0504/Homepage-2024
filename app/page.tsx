806c794ad42330f980a297f6559fabc0466fbe4c
Redish101 <i@redish101.top>
2024-03-13 20:52:43 +0800

    chore: add colors to `tailwind.config.ts`

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { faEnvelope, faKey } from "@fortawesome/free-solid-svg-icons";
 function ButtonPrimary({ text, link }: { text: string; link: string }) {
   return (
     <a href={link}>
-      <button className="bg-[#2F95FB] hover:bg-[#338AE2] transition-all ease-in-out px-9 py-2 text-white rounded-xl border-[1px] border-[#2F95FB]">
+      <button className="bg-primary hover:bg-primary-button-hover transition-all ease-in-out px-9 py-2 text-white rounded-xl border-[1px] border-primary">
         {text}
       </button>
     </a>
@@ -14,7 +14,7 @@ function ButtonPrimary({ text, link }: { text: string; link: string }) {
 function ButtonLight({ text, link }: { text: string; link: string }) {
   return (
     <a href={link}>
-      <button className="bg-[#E7F3FF] hover:bg-[#D5EAFF] transition-all ease-in-out px-9 py-2 text-[#1A8CFF] border-[1px] border-[#2F95FB] rounded-xl">
+      <button className="bg-light-button-bg hover:bg-light-button-hover transition-all ease-in-out px-9 py-2 text-light-button-text border-[1px] border-primary rounded-xl">
         {text}
       </button>
     </a>
@@ -30,7 +30,7 @@ export default function Home() {
           <div>
             I&apos;m{" "}
             <span
-              className="font-semibold relative z-10 after:absolute after:top-[50%] after:bg-[#A3CEF8]
+              className="font-semibold relative z-10 after:absolute after:top-[50%] after:bg-mark
                         after:w-[100%] after:h-[50%] after:left-0 after:z-[-1]"
             >
               Redish101
@@ -38,10 +38,10 @@ export default function Home() {
             .
           </div>
         </div>
-        <div className="text-2xl mt-3 text-[#4B4B4B] font-light">
+        <div className="text-2xl mt-3 text-light font-light">
           Student / Developer
         </div>
-        <div className="flex text-2x mt-3 text-[#6D6D6D] font-light">
+        <div className="flex text-2x mt-3 text-lightest font-light">
           <FontAwesomeIcon icon={faEnvelope} width={"16px"} />
           <span className="ml-2">i@redish101.top</span>
           <FontAwesomeIcon icon={faKey} width={"16px"} className="ml-3" />
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,7 +8,16 @@ const config: Config = {
   ],
   theme: {
     extend: {
-
+      colors: {
+        primary: "#2F95FB",
+        mark: "#A3CEF8",
+        light: "#4B4B4B",
+        lightest: "#6D6D6D",
+        "primary-button-hover": "#338AE2",
+        "light-button-bg": "#E7F3FF",
+        "light-button-hover": "#D5EAFF",
+        "light-button-text": "#1A8CFF"
+      }
     },
   },
   plugins: [],
