7de4dad96f93edcd6c179e01b7c79266df79c1af
Kegongteng <67720340+gtxykn0504@users.noreply.github.com>
2024-04-03 19:01:04 +0800

    layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ['latin']});
 
 export const metadata: Metadata = {
-  title: "Redish101",
+  title: "Kegongteng",
 };
 
 export default function RootLayout({
